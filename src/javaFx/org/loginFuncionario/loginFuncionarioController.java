7a83fd7c60a8791aa010ce9c67ec14abc87b71c8
Teus <mateus.vieira62@aluno.ifce.edu.br>
2024-09-06 20:55:33 -0300

    adicionando login funcionario

---
+++ src/javaFx/org/loginFuncionario/loginFuncionarioController.java
@@ -0,0 +1,22 @@
+package javaFx.org.loginFuncionario;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class loginFuncionarioController {
+
+    @FXML
+    private Button cadastrar;
+
+    @FXML
+    private Button entrar;
+
+    @FXML
+    private Button voltar;
+
+    @FXML
+    private TextField campo_cpf;
+
+
+}
